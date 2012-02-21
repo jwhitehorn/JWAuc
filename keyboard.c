7ed9048896c83260631207985abc9264f8512fae
Jason Whitehorn <jason.whitehorn@gmail.com>
2012-02-21 16:45:19 -0600

    changed the keyboard to be more of a mouse

--- keyboard.c
+++ keyboard.c
@@ -1,21 +1,44 @@
 #include <ApplicationServices/ApplicationServices.h>
 #include <unistd.h>
 #include <stdlib.h>
-//helper to simulate keyboard space bar pressing.
+//helper to simulate user input
 //gcc -o keyboard keyboard.c -Wall -framework ApplicationServices
 
 int main() {
-	int keyCode = 56;
+	int keyCode = 49; //space bar
 	srand ( time(NULL) );
-	printf("Keyboard SPACE keys coming in 30 seconds\n");
+	printf("User input coming in 30 seconds\n");
 	sleep(30);
 	while(1){
+		CGEventRef ourEvent = CGEventCreate(NULL);
+		CGPoint point = CGEventGetLocation(ourEvent);
+		
+	    // Left button down at 250x250
+	    CGEventRef click1_down = CGEventCreateMouseEvent(
+	        NULL, kCGEventLeftMouseDown,
+	        point,
+	        kCGMouseButtonLeft
+	    );
+	    CGEventRef click1_up = CGEventCreateMouseEvent(
+	        NULL, kCGEventLeftMouseUp,
+	        point,
+	        kCGMouseButtonLeft
+	    );
+		
+	    CGEventPost(kCGHIDEventTap, click1_down);
+	    sleep(0.11);			
+	    CGEventPost(kCGHIDEventTap, click1_up);
+		
+		CFRelease(click1_up);
+	    CFRelease(click1_down);
+		CFRelease(ourEvent);
+		/*
 		CGEventRef e = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)keyCode, true);
 		CGEventPost(kCGSessionEventTap, e);
 		CFRelease(e);
 		e = CGEventCreateKeyboardEvent (NULL, (CGKeyCode)keyCode, false);
 		CGEventPost(kCGSessionEventTap, e);		
-		printf(".\n");
+		printf(".\n");*/
 		//sleep((rand() % 1000) / 50.0 + 40.3);
 		sleep(10);
 	}
